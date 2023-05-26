54d68e4693fe4a1ecb00e263e4029b2f622b3703
Renan Afonso Batista <re.a.batista@gmail.com>
2023-05-26 01:45:22 -0300

    Correct split

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: r-afonso < r-afonso@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 15:35:31 by r-afonso          #+#    #+#             */
-/*   Updated: 2023/05/26 01:24:09 by r-afonso         ###   ########.fr       */
+/*   Updated: 2023/05/26 01:38:08 by r-afonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,24 +39,24 @@ char	**ft_split(char const *s, char c)
 	int		len;
 	int		index;
 
-	index = 0;
+	index = -1;
 	if (s == NULL)
 		return (NULL);
 	len_keywords = count_keywords((char *)s, c);
 	s_final = (char **)ft_calloc(len_keywords + 1, sizeof(char *));
 	if (s_final == NULL)
 		return (NULL);
-	while (len = 0, index < len_keywords)
+	while (index++, index < len_keywords)
 	{
+		len = 0;
 		while (*s && *s == c)
 			s++;
 		while (*(s + len) && *(s + len) != c)
 			len++;
 		*(s_final + index) = ft_substr(s, 0, len);
-		if(*(s_final + index) == NULL)
+		if (*(s_final + index) == NULL)
 			return (NULL);
 		s += len;
-		index++;
 	}
 	return (s_final);
 }
